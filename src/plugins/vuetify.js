0304e69a44dc4be74c91db5c4be6e98c0d74485b
Miggy <mendozajuanmiguel11@gmail.com>
2019-10-11 20:54:41 +0800

    added themes for the app

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,10 +1,25 @@
 import Vue from "vue";
 import Vuetify from "vuetify/lib";
+import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import colors from 'vuetify/lib/util/colors'
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
   icons: {
-    iconfont: "mdi"
+    iconfont: "md"
+  },
+  theme: {
+    themes: {
+      light: {
+        primary: "#D1243A" /*colors.red.darken2*/,
+        secondary: "#498B98",
+        accent: "#FFC857",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107"
+      }
+    }
   }
 });
