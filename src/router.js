376814f917dd371f771d22467c84ebec1aa69add
Miggy <mendozajuanmiguel11@gmail.com>
2019-10-11 22:35:35 +0800

    added landing login page route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
+import Login from "./views/Login.vue";
+import LandingLogin from "./views/LandingLogin.vue";
 
 Vue.use(Router);
 
@@ -10,17 +11,16 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
+      name: "login",
+      component: Login
     },
     {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
+      path: "/read",
+      name: "LandingLogin",
+      component: LandingLogin,
+    },
+    {
+      
     }
   ]
 });
