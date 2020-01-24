be38160766c9e5215c9ab2b287a053d87a850771
Miguel Mendoza <mendozajuanmiguel11@gmail.com>
2020-01-24 11:30:27 +0800

    updated API host link
    API: https://plm-loginatory-api.herokuapp.com/

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8081",
+  baseURL: "https://plm-loginatory-api.herokuapp.com/",
   headers: {
     "Content-type": "application/json",
   }
